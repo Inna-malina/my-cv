bf3f6af2734471e84df03d250a690a3778fd8fed
Inna Cherkashina <inna.cherkashina.1979@mail.ru>
2022-12-13 21:28:17 +0700

    feat:work of navElement changes; add animation block to portfolio.js

--- cv-november/src/modules/portfolio.js
+++ cv-november/src/modules/portfolio.js
@@ -1,6 +1,6 @@
 const portfolio = {
   "rs-school": {
-    "pet-story": {
+    "portfolio-1": {
       "top": "Истории о животных",
       "text": "Проект о животных. Имеет кроссбраузерную, адаптивную вёрстку. В проекте использовались карусели с плавной анимацией при перелистывани элементов; попапы в меню и в блоке отзывов. Так же на второй странице применялись интерактивные элементы при выборе суммы пожертвования.",
       "githubcode": "https://github.com/Inna-malina/rs-school/tree/zoo/zoo",
@@ -8,7 +8,7 @@ const portfolio = {
       "imgsrc": "assets/rs-school__1.jpg",
       "imgheight": "1805px"
     },
-    "song-film": {
+    "portfolio-2": {
       "top": "Песни из фильмов",
       "text": "Музыкальная викторина.Проект имеет кроссбраузерную, адаптивную вёрстку. Игра имеет несколько уровней. Все уровни заблокированы, их сменить самостоятельно нельзя. Когда на текущем уровне угадывается мелодия, тогда становится активной кнопка, которая даёт возможность перейти на следующий этап. В игре ведётся подсчёт и за каждую угаданную мелодию очки прибавляются, а за каждый неправильный ответ очки снимаются.",
       "githubcode": "https://github.com/Inna-malina/rs-school/tree/song-film/song-film",
@@ -16,7 +16,7 @@ const portfolio = {
       "imgsrc": "assets/rs-school__2.jpg",
       "imgheight": "350px"
     },
-    "portfolio": {
+    "portfolio-3": {
       "top": "Портфолио",
       "text": "Портфолио фотографа. Это одностраничный сайт. Имеет кроссбраузерную, адаптивную вёрстку. Есть смена языков, смена темы сайта (светлая тёмная). Сайт имеет фильтрацию фотографий по временам года",
       "githubcode": "https://github.com/Inna-malina/rs-school/tree/portfplio-photo/portfolio-photo",
@@ -24,10 +24,10 @@ const portfolio = {
       "imgsrc":"assets/rs-school__3.jpg",
       "imgheight": "1975px"
     },
-    "carousel":['carousel__1.jpg', 'carousel__2.jpg', 'carousel__3.jpg'],
+    "carousel":['assets/rs-school-carousel__1.jpg', 'assets/rs-school-carousel__2.jpg', 'assets/rs-school-carousel__3.jpg'],
   },
   "animation": {
-    "baikal": {
+    "portfolio-1": {
       "top": "Байкал",
       "text": "Проект для туристического агентства. Имеет кроссбраузерную, адаптивную вёрстку. Использовалась небольшая анимация: ховер эффекты, параллакс эффекты, 3-D элементы и т.д.",
       "githubcode": "https://github.com/Inna-malina/baikal-test",
@@ -35,15 +35,15 @@ const portfolio = {
       "imgsrc": "assets/animation__1.jpg",
       "imgheight": "1380px"
     },
-    "fashion-house": {
+    "portfolio-2": {
       "top": "Fashion House",
       "text": "Проект для магазина одежды",
       "githubcode": "https://github.com/Inna-malina/fashion-test",
       "linkproject": "https://inna-malina.github.io/fashion-test/",
-      "imgsrc": "assets/rs-school__1.jpg",
-      "imgheight": "1805px"
+      "imgsrc": "assets/animation__2.jpg",
+      "imgheight": "350px"
     },
-    "carousel":['carousel__1.jpg', 'carousel__2.jpg', 'carousel__3.jpg'],
+    "carousel":['assets/animation-carousel__1.jpg', 'assets/animation-carousel__2.jpg'],
   },
   "react": {
 
--- cv-november/src/modules/toChangePortfolio.js
+++ cv-november/src/modules/toChangePortfolio.js
@@ -1,7 +1,7 @@
 import portfolio from "./portfolio";
 
 export function toChangePortfolio() {
-  let carouselElements = document.querySelectorAll('.carousel-element');
+  // let carouselElements = document.querySelectorAll('.carousel-element');
   let linkContentImg = document.querySelector('.portfolio-content-img__box');
   let contentImg = document.querySelector('.portfolio-content-img__box img');
   let contentTitle = document.querySelector('.description-title a');
@@ -11,57 +11,109 @@ export function toChangePortfolio() {
 
   let navElems = document.querySelectorAll('.portfolio-nav__box ul li');
 
+  let carouselElements = document.querySelectorAll('.carousel-element');
+
   //Формируем основное оформление каждой страницы блока Мои работы (RS-School,Анимация,React,Разное)
-  navElems.forEach(function(element){
-    element.addEventListener('click', function(event){
+  navElems.forEach(function (navElem) {
+
+    navElem.addEventListener('click', function (event) {
       let action = event.target;
-      disabled(navElems);
-      action.classList.add('active');
+      disabledNav(navElems);
+      action.classList.add('active-nav');
+
       let elementPortfolio = portfolio[action.dataset.portfolio];
       // console.log(Object.values(elementPortfolio)[0]);      
       let info = Object.values(elementPortfolio)[0];
       linkContentImg.href = info.linkproject;
-      contentImg.src = info.imgsrc; 
-      contentImg.style.height = info.imgheight; 
+      contentImg.src = info.imgsrc;
+      contentImg.style.height = info.imgheight;
       contentTitle.textContent = info.top;
-      contentText.textContent = info.text; 
-      linkGithab.src = info.githubcode; 
-      linkProject.href = info.linkproject; 
+      contentText.textContent = info.text;
+      linkGithab.src = info.githubcode;
+      linkProject.href = info.linkproject;
 
 
+      //Создаём элементы карусели
+      let imagesCarousel = document.querySelectorAll('.carousel-element');
+      removeImg(imagesCarousel); //но перед созданием картинок нужно удалить предыдущий набор
+
+      let carouselField = document.querySelector('.carousel-field');
+      let imgCarousel = elementPortfolio.carousel;
+      for (let i = 1; i <= imgCarousel.length; i++) {
+        let imgElem = document.createElement('img');
+        imgElem.className = "carousel-element";
+        imgElem.dataset.portfolio = `portfolio-${i}`;
+        imgElem.dataset.container = action.dataset.portfolio;
+        imgElem.src = `assets/${action.dataset.portfolio}-carousel__${i}.jpg`;
+        carouselField.append(imgElem);
+        if (imgElem.dataset.portfolio == "portfolio-1") {
+          imgElem.classList.add('active-carousel');
+        }
+        imgElem.addEventListener('click', function (event) {
+          let action = event.target;
+          for (let key in portfolio) {
+            // key - "rs-school","animation","react","others" 
+            for (let elem in portfolio[key]) {
+              // elem - "pet-story","song-film","portfolio";
+              if (action.dataset.portfolio === elem && action.dataset.container === key) {
+                linkContentImg.href = portfolio[key][elem].linkproject;
+                contentImg.src = portfolio[key][elem].imgsrc;
+                contentImg.style.height = portfolio[key][elem].imgheight;
+                contentTitle.textContent = portfolio[key][elem].top;
+                contentText.textContent = portfolio[key][elem].text;
+                linkGithab.href = portfolio[key][elem].githubcode;
+                linkProject.href = portfolio[key][elem].linkproject;
+              }
+            }
+          }
+        });
+      }
     });
+
   });
 
-  //По клику на картинку карусели, происходит смена контента в блоке
+
+
+
+  // По клику на картинку карусели, происходит смена контента в блоке
   carouselElements.forEach(function (element) {
     element.addEventListener('click', function (event) {
       let action = event.target;
       disabled(carouselElements);
-      action.classList.add('active');
+      action.classList.add('active-carousel');
       for (let key in portfolio) {
-        //key - "rs-school","animation","react","others" 
-        for(let elem in portfolio[key]){
+        // key - "rs-school","animation","react","others" 
+        for (let elem in portfolio[key]) {
           // elem - "pet-story","song-film","portfolio";
-          if(action.dataset.portfolio === elem){
+          if (action.dataset.portfolio === elem && action.dataset.container === key) {
             linkContentImg.href = portfolio[key][elem].linkproject;
-            contentImg.src = portfolio[key][elem].imgsrc; 
-            contentImg.style.height = portfolio[key][elem].imgheight; 
+            contentImg.src = portfolio[key][elem].imgsrc;
+            contentImg.style.height = portfolio[key][elem].imgheight;
             contentTitle.textContent = portfolio[key][elem].top;
-            contentText.textContent = portfolio[key][elem].text; 
-            linkGithab.href = portfolio[key][elem].githubcode; 
-            linkProject.href = portfolio[key][elem].linkproject; 
+            contentText.textContent = portfolio[key][elem].text;
+            linkGithab.href = portfolio[key][elem].githubcode;
+            linkProject.href = portfolio[key][elem].linkproject;
           }
-          
         }
-       
       }
-
     });
   });
 
   function disabled(blocks) {
     blocks.forEach(function (element) {
-      element.classList.remove('active');
+      element.classList.remove('active-carousel');
+    });
+  }
+
+  function disabledNav(blocks) {
+    blocks.forEach(function (element) {
+      element.classList.remove('active-nav');
+    });
+  }
+
+  function removeImg(images) {
+    images.forEach(function (img) {
+      img.remove();
     });
   }
 }
