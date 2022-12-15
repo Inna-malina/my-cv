63cda7c203e9cb68e58e69a1ed58a22cbfc5175d
Inna Cherkashina <inna.cherkashina.1979@mail.ru>
2022-12-15 20:01:23 +0700

    feat: work on the screen--skills

--- cv-november/src/modules/toChangeScreen.js
+++ cv-november/src/modules/toChangeScreen.js
@@ -4,16 +4,26 @@ export function toChangeScreen() {
   let screens = document.querySelectorAll('.screen');
 
   menuElements.forEach(function (element) {
-    element.addEventListener('click', function () {
+    element.addEventListener('click', function (event) {
+      let action = event.target;
+      toDeleteMenu (menuElements);
+      action.classList.add('active-header-nav');
       screens.forEach(function (screen) {
         if(element.dataset.menu === screen.dataset.menu){
           screen.classList.remove('screen--hidden');
         }
         else{
           screen.classList.add('screen--hidden');
+          
         }
       });
     });
   });
+
+  function toDeleteMenu (elements){
+    elements.forEach(function(elem){
+      elem.classList.remove('active-header-nav');
+    });
+  }
 
 }
